918ca30871f06fe6c803e785ce93da1073525497
ArYTaN003 <aryan.tandon03@gmail.com>
2024-03-23 23:40:46 +0530

    Homework

--- Homework/Trees 1-2/Height_Of_Tree.cpp
+++ Homework/Trees 1-2/Height_Of_Tree.cpp
@@ -1,19 +1,5 @@
 int heightOfBinaryTree(TreeNode<int> *root)
 {
     if(root==NULL) return 0;
-    int height = 1;
-    queue<pair<TreeNode<int>*,int>> q;
-    q.push({root,1});
-    TreeNode<int> *f;
-    int l;
-    while(!q.empty()){
-        f = q.front().first;
-        l = q.front().second;
-        q.pop();
-        height = max(height,l);
-        if(f->left) q.push({f->left,l+1});
-        if(f->right) q.push({f->right,l+1});
-    }
-    return height;
-
+    return 1+max(heightOfBinaryTree(root->left),heightOfBinaryTree(root->right));
 }
